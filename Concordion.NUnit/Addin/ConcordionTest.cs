13a87c82c26414c11384910a8e64c70b3f539efb
jacek-ratzinger <jacek.ratzinger@gmx.at>
2014-03-14 13:11:28 +0100

    add specification and initial implementation idea

--- Concordion.NUnit/Addin/ConcordionTest.cs
+++ Concordion.NUnit/Addin/ConcordionTest.cs
@@ -15,9 +15,15 @@ namespace Concordion.Integration.NUnit.Addin
         private Type m_FixtureType;
 
         public ConcordionTest(Type fixtureType)
-            : base(string.Format("Executable Specification: {0}", fixtureType.Name))
+        //    : base(string.Format("Executable Specification: {0}", fixtureType.Name))
+            : base(fixtureType.FullName)
         {
             this.m_FixtureType = fixtureType;
+
+            this.fixtureSetUpMethods =
+                Reflect.GetMethodsWithAttribute(fixtureType, NUnitFramework.FixtureSetUpAttribute, true);
+            this.fixtureTearDownMethods =
+                Reflect.GetMethodsWithAttribute(fixtureType, NUnitFramework.FixtureTearDownAttribute, true);
         }
 
         #region Overrides of Test
@@ -25,6 +31,8 @@ namespace Concordion.Integration.NUnit.Addin
         public override TestResult Run(EventListener listener, ITestFilter filter)
         {
             listener.TestStarted(this.TestName);
+
+            RunSetup();
 
             var source = new EmbeddedResourceSource(m_FixtureType.Assembly);
             var target = new FileTarget(new SpecificationConfig().Load(m_FixtureType).BaseOutputDirectory);
@@ -34,9 +42,38 @@ namespace Concordion.Integration.NUnit.Addin
             var concordionResult = concordion.Process(Fixture);
             var testResult = NUnitTestResult(concordionResult);
 
+            RunTeardown();
+
             listener.TestFinished(testResult);
 
             return testResult;
+        }
+
+        private void RunFixtureSetUp()
+        {
+            if (setUpMethods != null)
+                foreach (MethodInfo setUpMethod in setUpMethods)
+                    Reflect.InvokeMethod(setUpMethod, setUpMethod.IsStatic ? null : this.Fixture);
+        }
+
+        private void RunFixtureTearDown(TestResult testResult)
+        {
+            try
+            {
+                if (tearDownMethods != null)
+                {
+                    int index = tearDownMethods.Length;
+                    while (--index >= 0)
+                        Reflect.InvokeMethod(tearDownMethods[index], tearDownMethods[index].IsStatic ? null : this.Fixture);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is NUnitException)
+                    ex = ex.InnerException;
+
+                RecordException(ex, testResult, FailureSite.TearDown);
+            }
         }
 
         private TestResult NUnitTestResult(IResultSummary concordionResult)
