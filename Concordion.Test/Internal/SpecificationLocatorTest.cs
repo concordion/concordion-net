5ee5c44e5cd1029ab021c067d7e90efb79e34210
jacek-ratzinger <jacek.ratzinger@gmx.at>
2014-04-10 15:30:58 +0200

    add test to find bug on removal of suffix 'Test'

---
+++ Concordion.Test/Internal/SpecificationLocatorTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Concordion.Api;
+using Concordion.Internal;
+using Concordion.NUnit.Addin;
+using NUnit.Framework;
+
+namespace Concordion.Test.Internal
+{
+    [TestFixture]
+    public class SpecificationLocatorTest
+    {
+        [Test]
+        public void ShouldRemoveTestSuffixes()
+        {
+            var specificationLocator = new ClassNameBasedSpecificationLocator();
+            var resource = specificationLocator.LocateSpecification(this);
+            var path = resource.Path.Replace(Path.DirectorySeparatorChar, '/');
+            Assert.AreEqual("Concordion/Test/Internal/SpecificationLocator.html", path, "path from SpecificationLocator contains 'Test'");
+        }
+
+        [Test]
+        public void ShouldNotRemoreWordTestInBetween()
+        {
+            var specificationLocator = new ClassNameBasedSpecificationLocator();
+            var resource = specificationLocator.LocateSpecification(new DummyContainingTestInNameTest());
+            var path = resource.Path.Replace(Path.DirectorySeparatorChar, '/');
+            Assert.AreEqual("Concordion/Test/Internal/DummyContainingTestInName.html", path, "path from SpecificiationLocator removed 'Test' in the middle");
+        }
+    }
+}
