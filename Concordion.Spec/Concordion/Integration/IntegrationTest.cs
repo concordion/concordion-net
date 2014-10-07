56cc89abb7886b5b9369cc14aa018656bfb6a5f3
jacek-ratzinger <jacek.ratzinger@gmx.at>
2014-10-07 12:57:02 +0200

    improve specifications on integration topics for publishing

---
+++ Concordion.Spec/Concordion/Integration/IntegrationTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Concordion.Integration;
+
+namespace Concordion.Spec.Concordion.Integration
+{
+    [ConcordionTest]
+    public class IntegrationTest
+    {
+    }
+}
