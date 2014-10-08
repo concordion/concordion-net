48cc384694fc7276e9c5fe25c6f6fca8f91b9468
jacek-ratzinger <jacek.ratzinger@gmx.at>
2014-10-08 16:49:57 +0200

    fix broken links in specifications

---
+++ Concordion.Spec/Concordion/Results/AssertTrue/Success/SuccessTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Concordion.Integration;
+
+namespace Concordion.Spec.Concordion.Results.AssertTrue.Success
+{
+    [ConcordionTest]
+    public class SuccessTest
+    {
+        public bool isPalindrome(string s)
+        {
+            return s == Reverse(s);
+        }
+
+        private string Reverse(string s)
+        {
+            var builder = new StringBuilder();
+
+            for (int index = s.Length - 1; index >= 0; index--)
+            {
+                builder.Append(s[index]);
+            }
+
+            return builder.ToString();
+        }
+
+        public string render(string fragment)
+        {
+            return new TestRig()
+                .WithFixture(this)
+                .ProcessFragment(fragment)
+                .GetOutputFragmentXML();
+        }
+    }
+}
