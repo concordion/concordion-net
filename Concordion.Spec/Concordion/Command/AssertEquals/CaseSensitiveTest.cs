f78454a1f24615565dff9090ddc5cbbcfc659dcb
jacek-ratzinger <jacek.ratzinger@gmx.at>
2014-12-24 10:22:07 +0100

    use documentation as specification for assertequals

---
+++ Concordion.Spec/Concordion/Command/AssertEquals/CaseSensitiveTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Concordion.Integration;
+using Concordion.Spec.Support;
+
+namespace Concordion.Spec.Concordion.Command.AssertEquals
+{
+    [ConcordionTest]
+    public class CaseSensitiveTest
+    {
+        public string successOrFailure(string fragment, string evaluationResult)
+        {
+            return new TestRig()
+                .WithStubbedEvaluationResult(evaluationResult)
+                .ProcessFragment(fragment)
+                .SuccessOrFailureInWords();
+        }
+    }
+}
