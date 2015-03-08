7dc32ab4bb43b67de7b35f25b90541813c42d2ce
ShaKaRee <concordion8@gmail.com>
2015-03-08 15:03:45 +0100

    remove Concordion Gallio adapter (Gallio has not been released since July 2012 - 3.4.14.0)

--- run-spec-with-echo.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-echo !!! You must have Gallio installed and have Gallio.Echo.exe in the PATH to run this batch file !!!
-Gallio.Echo.exe /pd:Gallio-Concordion-Adapter /runner:Local /wd:. Specifications\Concordion.Spec.dll
-pause
--- run-unit-test-with-echo.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-echo !!! You must have Gallio installed and have Gallio.Echo.exe in the PATH to run this batch file !!!
-Gallio.Echo.exe /runner:Local Test\Concordion.Test.dll
-pause
