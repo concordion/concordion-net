e87c062b7cf0d9c46319d68828a4f2be2229592d
KidFashion <KidFashion@@users.noreply.github.com>
2017-08-20 11:04:50 +0300

    Added version compiling targetting .NET Standard 2.0

--- Concordion/Internal/FileTarget.cs
+++ Concordion/Internal/FileTarget.cs
@@ -15,7 +15,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Policy;
+//using System.Security.Policy;
 using System.Text;
 using Concordion.Api;
 using System.IO;
